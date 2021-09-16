fd362719e78d61effb9d74e157c45062404cd2b1
kibra <62299912+kibradev@users.noreply.github.com>
2021-09-16 15:25:08 +0300

    Delete SERVER-DATA/resources/[Standalone]/ld-vehicleshop directory

--- SERVER-DATA/resources/[Standalone]/ld-vehicleshop/locales/en.lua
+++
@@ -1,8 +0,0 @@
-Locales ['en'] = {
-  ['press_to_open'] = "~r~E~w~ Basarak Araç Mağazasına Erişin",
-  ['enough_money'] = 'Paranız Yetersiz',
-  ['we_dont_vehicle'] = 'Bu araç size ait değil.',
-  ['car_dealer'] = 'Otomotiv Bayiliği',
-  ['rotate_keys'] = 'Aracı döndürmek için A ve D yi kullanın.',
-  ['testdrive'] = 'Test Sürüşünün Bitmesine Kalan süre: '
- }
